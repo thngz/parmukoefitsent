e7a56b05bb973c65cd41672cf0a8d7b5defb165f
gkiviv <georgkivivali7@gmail.com>
2024-06-18 20:24:57 +0300

    improved the worker

--- App.Infrastructure/Workers/RimiWorker.cs
+++ App.Infrastructure/Workers/RimiWorker.cs
@@ -5,6 +5,7 @@ using App.DAL;
 using App.Infrastructure.Interfaces;
 using App.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -15,7 +16,7 @@ namespace App.Infrastructure.Workers;
 public class RimiUrl
 {
     private int _currentPage = 1;
-    private int _pageSize = 20;
+    private int _pageSize = 80;
     private const string BaseUrl = "https://www.rimi.ee/epood/ee/tooted/alkohol/c/SH-1?";
 
     public string GetUrl(int index)
@@ -27,14 +28,14 @@ public class RimiUrl
 public class RimiWorker : IScrapeWorker
 {
     private readonly RimiUrl _url = new();
-    private readonly AppDbContext _context;
+    private readonly IServiceProvider _serviceProvider;
     private readonly ILogger<RimiWorker> _logger;
     private readonly Store _store;
 
-    public RimiWorker(AppDbContext context, ILogger<RimiWorker> logger)
+    public RimiWorker(IServiceProvider serviceProvider, ILogger<RimiWorker> logger)
     {
-        _context = context;
         _logger = logger;
+        _serviceProvider = serviceProvider;
         _store = GetStore().Result;
     }
 
@@ -45,36 +46,53 @@ public class RimiWorker : IScrapeWorker
         var paginationList = initialDriver.FindElement(By.ClassName("pagination__list"));
         var paginationItems = paginationList.FindElements(By.ClassName("pagination__item"));
         var lastPage = int.Parse(paginationItems.ElementAt(paginationItems.Count - 2).Text);
-
-        var _lock = new object();
         initialDriver.Quit();
-        
-        // let each thread work on a range
-        Parallel.For(1, lastPage, new ParallelOptions { MaxDegreeOfParallelism = 6 }, index =>
+
+        Parallel.For(1,
+            lastPage,
+            new ParallelOptions { MaxDegreeOfParallelism = 6 },
+            StartProcessingPages);
+    }
+
+    private void StartProcessingPages(int index)
+    {
+        var driver = CreateDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        driver.Navigate().GoToUrl(_url.GetUrl(index));
+        ClickCookieBanner(driver);
+        _logger.LogInformation($"starting {index}th page ");
+        var products = GetProductsOnPage(driver);
+
+        using (var scope = _serviceProvider.CreateScope())
         {
-            var driver = CreateDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Navigate().GoToUrl(_url.GetUrl(index));
-            ClickCookieBanner(driver);
-            _logger.LogInformation($"starting {index}th page ");
-            var products = GetProductsOnPage(driver);
+            var ctx = scope.ServiceProvider.GetService<AppDbContext>();
             foreach (var product in products)
             {
                 _logger.LogInformation($"Visiting {product.ProductUrl}");
-                driver.Navigate().GoToUrl(product.ProductUrl);
-            
-                CalculateCoefficient(product, driver);
-            
-                _logger.LogInformation($"{product.Name} object made");
+                var existing = ctx.Products.FirstOrDefault(p => p.ProductUrl == product.ProductUrl);
+
+                if (existing is null)
+                {
+                    driver.Navigate().GoToUrl(product.ProductUrl);
+                    GetAdditionalData(product, driver);
+
+                    _logger.LogInformation($"{product.Name} coefficient calculated");
+                }
+                else
+                {
+                    product.AlcContent = existing.AlcContent;
+                    product.Amount = existing.Amount;
+                }
+
+                product.Coefficient = Math.Round(product.AlcContent * product.Amount / product.Price, 2);
             }
 
-            lock (_lock)
-            {
-                UpsertProducts(products); 
-                _context.SaveChanges();
-            }
-            driver.Quit();
-        });
+
+            UpsertProducts(products, ctx);
+            ctx.SaveChanges();
+        }
+
+        driver.Quit();
     }
 
 
@@ -105,7 +123,7 @@ public class RimiWorker : IScrapeWorker
         return product;
     }
 
-    private void CalculateCoefficient(Product product, IWebDriver driver)
+    private void GetAdditionalData(Product product, IWebDriver driver)
     {
         var productDetails = new ReadOnlyCollection<IWebElement>(new List<IWebElement>());
         try
@@ -116,7 +134,7 @@ public class RimiWorker : IScrapeWorker
         {
             _logger.LogError("Trying again!");
             driver.Navigate().Refresh();
-            CalculateCoefficient(product, driver);
+            GetAdditionalData(product, driver);
         }
 
         foreach (var productDetail in productDetails)
@@ -134,15 +152,13 @@ public class RimiWorker : IScrapeWorker
                 product.AlcContent = decimal.Parse(vol);
             }
         }
-
-        product.Coefficient = Math.Round(product.AlcContent * product.Amount / product.Price, 2);
     }
 
 
-    private void UpsertProducts(IEnumerable<Product> products)
+    private void UpsertProducts(IEnumerable<Product> products, AppDbContext context)
     {
         _logger.LogInformation("Upserting products");
-        _context.Products.UpsertRange(products).On(p => p.ProductUrl).Run();
+        context.Products.UpsertRange(products).On(p => p.ProductUrl).Run();
     }
 
     private void ClickCookieBanner(IWebDriver driver)
@@ -163,13 +179,16 @@ public class RimiWorker : IScrapeWorker
 
     private async Task<Store> GetStore()
     {
-        var store = _context.Stores.FirstOrDefault(s => s.Name == "Rimi");
+        using var scope = _serviceProvider.CreateScope();
+
+        var ctx = scope.ServiceProvider.GetService<AppDbContext>();
+        var store = ctx.Stores.FirstOrDefault(s => s.Name == "Rimi");
 
         if (store is null)
         {
-            _context.Stores.Add(new Store { Name = "Rimi" });
-            await _context.SaveChangesAsync();
-            store = _context.Stores.FirstOrDefault(s => s.Name == "Rimi");
+            ctx.Stores.Add(new Store { Name = "Rimi" });
+            await ctx.SaveChangesAsync();
+            store = ctx.Stores.FirstOrDefault(s => s.Name == "Rimi");
         }
 
         return store;
